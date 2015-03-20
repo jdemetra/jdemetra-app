a935fb88e671475fd578e7e70e80f422c2685ab8
Thomas Witthohn <thomas.witthohn@bundesbank.de>
2015-03-20 14:27:00 +0100

    Fixing ReloadableImpl in DataSetNode and DataSourceNode

--- nbdemetra-ui/src/main/java/ec/nbdemetra/ui/tsproviders/DataSourceNode.java
+++ nbdemetra-ui/src/main/java/ec/nbdemetra/ui/tsproviders/DataSourceNode.java
@@ -1,26 +1,26 @@
 /*
  * Copyright 2013 National Bank of Belgium
- * 
- * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
  * by the European Commission - subsequent versions of the EUPL (the "Licence");
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl
- * 
- * Unless required by applicable law or agreed to in writing, software 
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and 
+ * See the Licence for the specific language governing permissions and
  * limitations under the Licence.
  */
 package ec.nbdemetra.ui.tsproviders;
 
 import com.google.common.base.Optional;
 import ec.nbdemetra.ui.Config;
-import ec.nbdemetra.ui.interchange.Exportable;
 import ec.nbdemetra.ui.INameable;
 import ec.nbdemetra.ui.IReloadable;
+import ec.nbdemetra.ui.interchange.Exportable;
 import ec.nbdemetra.ui.nodes.FailSafeChildFactory;
 import ec.nbdemetra.ui.nodes.NodeAnnotator;
 import ec.nbdemetra.ui.nodes.Nodes;
@@ -187,7 +187,11 @@ public final class DataSourceNode extends AbstractNode {
 
         @Override
         public void reload() {
-            setChildren(Children.create(new DataSourceChildFactory(getLookup().lookup(DataSource.class)), true));
+            DataSource dataSource = getLookup().lookup(DataSource.class);
+            IDataSourceProvider provider = TsProviders.lookup(IDataSourceProvider.class, dataSource).get();
+
+            provider.reload(dataSource);
+            setChildren(Children.create(new DataSourceChildFactory(dataSource), true));
         }
     }
 
