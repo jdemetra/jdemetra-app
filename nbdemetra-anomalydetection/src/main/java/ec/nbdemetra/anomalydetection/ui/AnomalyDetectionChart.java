5f6825a39fce82facd4a9ffe38f4f992cfcaf75c
Philippe Charles <philippe.charles@nbb.be>
2015-06-18 16:48:47 +0200

    Improved anomaly detection chart.

---
+++ nbdemetra-anomalydetection/src/main/java/ec/nbdemetra/anomalydetection/ui/AnomalyDetectionChart.java
@@ -0,0 +1,243 @@
+/*
+ * Copyright 2015 National Bank of Belgium
+ * 
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ * by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and 
+ * limitations under the Licence.
+ */
+package ec.nbdemetra.anomalydetection.ui;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.FluentIterable;
+import ec.nbdemetra.ui.ThemeSupport;
+import ec.nbdemetra.ui.properties.l2fprod.ColorChooser;
+import ec.tss.Ts;
+import ec.tss.datatransfer.TssTransferSupport;
+import ec.tstoolkit.data.Table;
+import ec.tstoolkit.modelling.arima.PreprocessingModel;
+import ec.tstoolkit.timeseries.regression.OutlierEstimation;
+import ec.tstoolkit.timeseries.simplets.TsData;
+import ec.tstoolkit.timeseries.simplets.TsPeriod;
+import ec.ui.chart.TsXYDatasets;
+import ec.util.chart.ColorScheme;
+import ec.util.chart.ObsFunction;
+import ec.util.chart.ObsPredicate;
+import ec.util.chart.SeriesFunction;
+import ec.util.chart.SeriesPredicate;
+import ec.util.chart.swing.JTimeSeriesChart;
+import static ec.util.chart.swing.JTimeSeriesChartCommand.copyImage;
+import static ec.util.chart.swing.JTimeSeriesChartCommand.printImage;
+import static ec.util.chart.swing.JTimeSeriesChartCommand.saveImage;
+import ec.util.various.swing.JCommand;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Transferable;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.NumberFormat;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.swing.JComponent;
+import javax.swing.JMenu;
+
+/**
+ *
+ * @author Philippe Charles
+ */
+final class AnomalyDetectionChart extends JComponent {
+
+    public static final String MODEL_PROPERTY = "model";
+
+    private final JTimeSeriesChart chart;
+    private final ThemeSupport themeSupport;
+    private Model model;
+
+    public AnomalyDetectionChart() {
+        this.chart = new JTimeSeriesChart();
+        this.themeSupport = ThemeSupport.create(chart);
+
+        chart.setLegendVisibilityPredicate(SeriesPredicate.alwaysFalse());
+        chart.setSeriesColorist(new SeriesFunction<Color>() {
+            @Override
+            public Color apply(int series) {
+                return chart.getColorSchemeSupport().getLineColor(ColorScheme.KnownColor.GRAY);
+            }
+        });
+        chart.setObsFormatter(new ObsFunction<String>() {
+            @Override
+            public String apply(int series, int obs) {
+                TsData data = model.getTs().getTsData();
+                TsPeriod period = data.getDomain().get(obs);
+                double value = data.get(obs);
+                NumberFormat valueFormat = chart.getValueFormat();
+                OutlierEstimation outlier = model.outliers[obs];
+                return outlier != null
+                        ? new StringBuilder()
+                        .append("Period : ").append(period).append('\n')
+                        .append("Value : ").append(valueFormat.format(value)).append('\n')
+                        .append("Outlier Value : ").append(valueFormat.format(outlier.getValue())).append('\n')
+                        .append("Std Err : ").append(valueFormat.format(outlier.getStdev())).append('\n')
+                        .append("TStat : ").append(valueFormat.format(outlier.getTStat())).append('\n')
+                        .append("Outlier type : ").append(outlier.getCode()).toString()
+                        : new StringBuilder()
+                        .append("Period : ").append(period).append('\n')
+                        .append("Value : ").append(valueFormat.format(value)).toString();
+            }
+        });
+        chart.setObsHighlighter(new ObsPredicate() {
+            @Override
+            public boolean apply(int series, int obs) {
+                return model.outliers[obs] != null || chart.getHoveredObs().equals(series, obs);
+            }
+        });
+        chart.setObsColorist(new ObsFunction<Color>() {
+            @Override
+            public Color apply(int series, int obs) {
+                OutlierEstimation outlier = model.outliers[obs];
+                return outlier != null
+                        ? ColorChooser.getColor(outlier.getCode())
+                        : chart.getSeriesColorist().apply(series);
+            }
+        });
+        chart.setComponentPopupMenu(newMenu().getPopupMenu());
+
+        addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                switch (evt.getPropertyName()) {
+                    case MODEL_PROPERTY:
+                        onModelChange();
+                        break;
+                }
+            }
+        });
+        themeSupport.register();
+
+        setLayout(new BorderLayout());
+        add(chart, BorderLayout.CENTER);
+    }
+
+    private void onModelChange() {
+        chart.setDataset(model != null ? TsXYDatasets.from(model.getTs()) : null);
+    }
+
+    @Nullable
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(@Nullable Model model) {
+        Model old = this.model;
+        this.model = model;
+        firePropertyChange(MODEL_PROPERTY, old, this.model);
+    }
+
+    public static final class Model {
+
+        private final Ts ts;
+        private final OutlierEstimation[] outliers;
+
+        Model(@Nonnull Ts ts, @Nonnull PreprocessingModel model) {
+            this.ts = ts;
+            TsData data = ts.getTsData();
+            this.outliers = new OutlierEstimation[data.getLength()];
+            TsPeriod start = data.getStart();
+            for (OutlierEstimation o : model.outliersEstimation(true, false)) {
+                outliers[o.getPosition().minus(start)] = o;
+            }
+        }
+
+        @Nonnull
+        public Ts getTs() {
+            return ts;
+        }
+
+        @Nonnull
+        public OutlierEstimation[] getOutliers() {
+            return outliers;
+        }
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="Menus">
+    private JMenu newMenu() {
+        JMenu result = new JMenu();
+
+        result.add(new CopyData().toAction(this)).setText("Copy data");
+        result.add(new CopyOutliers().toAction(this)).setText("Copy outliers");
+        result.addSeparator();
+        result.add(newExportMenu());
+
+        return result;
+    }
+
+    private JMenu newExportMenu() {
+        JMenu result = new JMenu("Export image to");
+        result.add(printImage().toAction(chart)).setText("Printer...");
+        result.add(copyImage().toAction(chart)).setText("Clipboard");
+        result.add(saveImage().toAction(chart)).setText("File...");
+        return result;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Commands">
+    private static abstract class ModelCommand extends JCommand<AnomalyDetectionChart> {
+
+        @Override
+        public boolean isEnabled(AnomalyDetectionChart c) {
+            return c.getModel() != null;
+        }
+
+        @Override
+        public JCommand.ActionAdapter toAction(AnomalyDetectionChart c) {
+            return super.toAction(c).withWeakPropertyChangeListener(c, MODEL_PROPERTY);
+        }
+    }
+
+    private static final class CopyData extends ModelCommand {
+
+        @Override
+        public void execute(AnomalyDetectionChart c) throws Exception {
+            Transferable t = TssTransferSupport.getDefault().fromTs(c.model.getTs());
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(t, null);
+        }
+    }
+
+    private static final class CopyOutliers extends ModelCommand {
+
+        @Override
+        public void execute(AnomalyDetectionChart c) throws Exception {
+            Transferable t = TssTransferSupport.getDefault().fromTable(toTable(c.model.getOutliers()));
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(t, null);
+        }
+
+        private Table<Object> toTable(OutlierEstimation[] input) {
+            OutlierEstimation[] tmp = FluentIterable.of(input).filter(Predicates.notNull()).toArray(OutlierEstimation.class);
+            Table<Object> result = new Table<>(tmp.length + 1, 5);
+            result.set(0, 0, "");
+            result.set(0, 1, "Period");
+            result.set(0, 2, "Value");
+            result.set(0, 3, "StdErr");
+            result.set(0, 4, "TStat");
+            for (int i = 0; i < tmp.length; i++) {
+                OutlierEstimation o = tmp[i];
+                result.set(i + 1, 0, o.getCode());
+                result.set(i + 1, 1, o.getPosition());
+                result.set(i + 1, 2, o.getValue());
+                result.set(i + 1, 3, o.getStdev());
+                result.set(i + 1, 4, o.getTStat());
+            }
+            return result;
+        }
+    }
+    //</editor-fold>
+}
