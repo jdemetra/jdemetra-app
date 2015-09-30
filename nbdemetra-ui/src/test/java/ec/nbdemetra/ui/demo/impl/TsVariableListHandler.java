766e273257a4277b3501acc89bbab2ce22d949ab
Philippe Charles <philippe.charles@nbb.be>
2015-09-30 09:53:13 +0200

    Added display of regression variables.

---
+++ nbdemetra-ui/src/test/java/ec/nbdemetra/ui/demo/impl/TsVariableListHandler.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2015 National Bank of Belgium
+ * 
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ * by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and 
+ * limitations under the Licence.
+ */
+package ec.nbdemetra.ui.demo.impl;
+
+import ec.nbdemetra.ui.demo.DemoComponentHandler;
+import ec.nbdemetra.ui.demo.DemoTsActions;
+import ec.ui.list.JTsVariableList;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Philippe Charles
+ */
+@ServiceProvider(service = DemoComponentHandler.class)
+public final class TsVariableListHandler extends DemoComponentHandler.InstanceOf<JTsVariableList> {
+
+    public TsVariableListHandler() {
+        super(JTsVariableList.class);
+    }
+
+    @Override
+    public void doConfigure(JTsVariableList c) {
+        c.setTsAction(DemoTsActions.SHOW_DIALOG);
+    }
+}
