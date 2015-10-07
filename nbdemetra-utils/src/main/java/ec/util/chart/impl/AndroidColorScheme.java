8234e697445c0a5a59bea4983716832bcd6003fe
Laurent Jadoul <laurent.jadoul@nbb.be>
2015-10-07 10:47:32 +0200

    Add Display Name for android color scheme.

--- nbdemetra-utils/src/main/java/ec/util/chart/impl/AndroidColorScheme.java
+++ nbdemetra-utils/src/main/java/ec/util/chart/impl/AndroidColorScheme.java
@@ -1,20 +1,19 @@
 /*
-* Copyright 2013 National Bank of Belgium
-*
-* Licensed under the EUPL, Version 1.1 or – as soon they will be approved 
-* by the European Commission - subsequent versions of the EUPL (the "Licence");
-* You may not use this work except in compliance with the Licence.
-* You may obtain a copy of the Licence at:
-*
-* http://ec.europa.eu/idabc/eupl
-*
-* Unless required by applicable law or agreed to in writing, software 
-* distributed under the Licence is distributed on an "AS IS" basis,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the Licence for the specific language governing permissions and 
-* limitations under the Licence.
-*/
-
+ * Copyright 2013 National Bank of Belgium
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved 
+ * by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://ec.europa.eu/idabc/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and 
+ * limitations under the Licence.
+ */
 package ec.util.chart.impl;
 
 import ec.util.chart.ColorSchemeSupport;
@@ -81,12 +80,17 @@ public abstract class AndroidColorScheme extends AbstractColorScheme {
         public String getName() {
             return "Android - Dark";
         }
+        
+        @Override
+        public String getDisplayName() {
+            return "Ice Cream Sandwich - Dark";
+        }
 
         @Override
         public int getBackColor() {
             return BasicColor.WHITE;
         }
-        
+
         @Override
         public int getPlotColor() {
             return ColorSchemeSupport.rgb(10, 10, 10);
@@ -113,10 +117,15 @@ public abstract class AndroidColorScheme extends AbstractColorScheme {
         }
 
         @Override
+        public String getDisplayName() {
+            return "Ice Cream Sandwich - Light";
+        }
+
+        @Override
         public int getBackColor() {
             return BasicColor.WHITE;
         }
-        
+
         @Override
         public int getPlotColor() {
             return BasicColor.WHITE;
