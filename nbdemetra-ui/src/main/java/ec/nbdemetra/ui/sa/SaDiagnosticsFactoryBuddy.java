daf07301626970a702c586a943cf6a1f73f4fb50
Philippe Charles <philippe.charles@nbb.be>
2015-10-08 14:43:05 +0200

    Added support of configuration of diagnostics.

---
+++ nbdemetra-ui/src/main/java/ec/nbdemetra/ui/sa/SaDiagnosticsFactoryBuddy.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 National Bank of Belgium
+ * 
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ * by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and 
+ * limitations under the Licence.
+ */
+package ec.nbdemetra.ui.sa;
+
+import ec.nbdemetra.ui.DemetraUiIcon;
+import ec.nbdemetra.ui.ns.INamedService;
+import ec.tstoolkit.design.ServiceDefinition;
+import java.awt.Image;
+import org.openide.nodes.Sheet;
+import org.openide.util.ImageUtilities;
+
+/**
+ *
+ * @author Philippe Charles
+ */
+@ServiceDefinition(hasPosition = true)
+public abstract class SaDiagnosticsFactoryBuddy implements INamedService {
+
+    @Override
+    public String getDisplayName() {
+        return getName();
+    }
+
+    @Override
+    public Image getIcon(int type, boolean opened) {
+        return ImageUtilities.icon2Image(DemetraUiIcon.PUZZLE_16);
+    }
+
+    @Override
+    public Sheet createSheet() {
+        return new Sheet();
+    }
+}
