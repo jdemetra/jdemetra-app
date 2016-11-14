edd538404e7898fb72dce847a3b9231625311933
Philippe Charles <philippe.charles@nbb.be>
2016-11-14 17:33:02 +0100

    Fixed NPE when getting icon for missing provider.

--- nbdemetra-ui/src/main/java/ec/nbdemetra/ui/mru/ProviderMruAction.java
+++ nbdemetra-ui/src/main/java/ec/nbdemetra/ui/mru/ProviderMruAction.java
@@ -21,6 +21,7 @@ import ec.nbdemetra.ui.tsproviders.DataSourceProviderBuddySupport;
 import ec.tss.tsproviders.DataSource;
 import ec.tss.tsproviders.IDataSourceLoader;
 import ec.tss.tsproviders.TsProviders;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.beans.BeanInfo;
 import java.beans.PropertyChangeEvent;
@@ -114,7 +115,8 @@ public final class ProviderMruAction extends AbstractAction implements Presenter
         }
 
         Icon getIcon(DataSourceProviderBuddySupport support, DataSource dataSource) {
-            return ImageUtilities.image2Icon(support.get(dataSource).getIcon(dataSource, BeanInfo.ICON_COLOR_16x16, false));
+            Image result = support.get(dataSource).getIcon(dataSource, BeanInfo.ICON_COLOR_16x16, false);
+            return result != null ? ImageUtilities.image2Icon(result) : null;
         }
 
         boolean isLoadable(DataSource dataSource) {
