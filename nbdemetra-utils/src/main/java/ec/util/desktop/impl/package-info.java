a0e892b001f1879e0f18d0f0dbd737028e2589a4
Philippe Charles <philippe.charles@nbb.be>
2018-04-24 16:56:47 +0200

    Moved project nbdemetra-utils to its own repository.

--- nbdemetra-utils/src/main/java/ec/util/desktop/impl/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains some Desktop implementations. It is not part of the
- * Desktop API but may be bundled with it.<p>This package brings support for Mac
- * OS X, Windows and Linux.
- */
-package ec.util.desktop.impl;
