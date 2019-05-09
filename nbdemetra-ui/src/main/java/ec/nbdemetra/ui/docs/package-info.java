5b8c62d0634df9a1278fa90672a8846ebee0dd35
Philippe Charles <philippe.charles@nbb.be>
2019-05-09 16:48:56 +0200

    Bump NetBeans platform version to 11.0

--- nbdemetra-ui/src/main/java/ec/nbdemetra/ui/docs/package-info.java
+++ nbdemetra-ui/src/main/java/ec/nbdemetra/ui/docs/package-info.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-@HelpSetRegistration(helpSet = "ui-hs.xml", position = 3858)
+//@HelpSetRegistration(helpSet = "ui-hs.xml", position = 3858)
 package ec.nbdemetra.ui.docs;
 
-import org.netbeans.api.javahelp.HelpSetRegistration;
+//import org.netbeans.api.javahelp.HelpSetRegistration;
