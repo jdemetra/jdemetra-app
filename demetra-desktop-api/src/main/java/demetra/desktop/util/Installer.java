3301ee4ea94a10170c67a4983dba5133fbf163d1
Philippe Charles <philippe.charles@nbb.be>
2022-03-21 11:41:59 +0100

    Restore paths and MRU persistence

--- demetra-desktop-api/src/main/java/demetra/desktop/util/Installer.java
+++ demetra-desktop-api/src/main/java/demetra/desktop/util/Installer.java
@@ -18,6 +18,8 @@ package demetra.desktop.util;
 
 import demetra.timeseries.TsProvider;
 import demetra.desktop.TsManager;
+import demetra.desktop.ui.mru.MruProvidersStep;
+import demetra.desktop.ui.mru.MruWorkspacesStep;
 import demetra.desktop.workspace.WorkspaceFactory;
 import demetra.tsprovider.FileLoader;
 import java.io.File;
@@ -25,22 +27,31 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import nbbrd.io.text.Formatter;
+import nbbrd.io.text.Parser;
+import nbbrd.io.xml.bind.Jaxb;
 import org.openide.modules.ModuleInstall;
 import org.openide.util.Lookup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class Installer extends ModuleInstall{
+public final class Installer extends ModuleInstall {
 
     final static Logger LOGGER = LoggerFactory.getLogger(Installer.class);
 
     public static final InstallerStep STEP = InstallerStep.all(
             new AppVersionStep(),
             new ProvidersV3Step(),
-            new TsVariableStep());
+            new TsVariableStep(),
+            new MruProvidersStep(),
+            new MruWorkspacesStep()
+    );
 
     @Override
     public void restored() {
@@ -58,7 +69,7 @@ public final class Installer extends ModuleInstall{
     public boolean closing() {
         return WorkspaceFactory.getInstance().closeWorkspace(true);
     }
-    
+
     private static final class AppVersionStep extends InstallerStep {
 
         @Override
@@ -77,8 +88,8 @@ public final class Installer extends ModuleInstall{
     private static final class ProvidersV3Step extends InstallerStep.LookupStep<TsProvider> {
 
         final Preferences prefs = prefs();
-//        final Parser<File[]> pathsParser = Parsers.onJAXB(PathsBean.class).andThen(o -> o.paths != null ? o.paths : new File[0])::parse;
-//        final Formatter<File[]> pathsFormatter = Formatters.onJAXB(PathsBean.class, false).compose(PathsBean::create)::format;
+        final Parser<File[]> pathsParser = Jaxb.Parser.of(PathsBean.class).asParser().andThen(o -> o.paths != null ? o.paths : new File[0]);
+        final Formatter<File[]> pathsFormatter = Jaxb.Formatter.of(PathsBean.class).asFormatter().compose(PathsBean::create);
 
         ProvidersV3Step() {
             super(TsProvider.class);
@@ -89,19 +100,19 @@ public final class Installer extends ModuleInstall{
             for (TsProvider o : providers) {
                 TsManager.getDefault().register(o);
                 if (o instanceof FileLoader) {
-//                    tryGet(pathsNode, o.getSource(), pathsParser)
-//                            .ifPresent(((FileLoader) o)::setPaths);
+                    tryGet(pathsNode, o.getSource(), pathsParser)
+                            .ifPresent(((FileLoader) o)::setPaths);
                 }
             }
 //            TsManager.getDefault().register(new PocProvider());
         }
 
         private void unregister(Iterable<? extends TsProvider> providers) {
-//            Preferences pathsNode = prefs.node("paths");
+            Preferences pathsNode = prefs.node("paths");
             for (TsProvider o : providers) {
-//                if (o instanceof FileLoader) {
-//                    tryPut(pathsNode, o.getSource(), pathsFormatter, ((FileLoader) o).getPaths());
-//                }
+                if (o instanceof FileLoader) {
+                    tryPut(pathsNode, o.getSource(), pathsFormatter, ((FileLoader) o).getPaths());
+                }
                 TsManager.getDefault().unregister(o);
             }
         }
@@ -136,112 +147,28 @@ public final class Installer extends ModuleInstall{
         @Override
         protected void onClose(Lookup.Result<TsProvider> lookup) {
             unregister(TsManager.getDefault().getProviders().collect(Collectors.toList()));
-//            try {
-//                prefs.flush();
-//            } catch (BackingStoreException ex) {
-//                LOGGER.warn("Can't flush storage", ex);
-//            }
+            try {
+                prefs.flush();
+            } catch (BackingStoreException ex) {
+                LOGGER.warn("Can't flush storage", ex);
+            }
             TsManager.getDefault().close();
         }
 
-//        @XmlRootElement(name = "paths")
-//        @XmlJavaTypeAdapter(value = FileXmlAdapter.class, type = File.class)
-//        static class PathsBean {
-//
-//            @XmlElement(name = "path")
-//            public File[] paths;
-//
-//            static PathsBean create(File[] o) {
-//                PathsBean result = new PathsBean();
-//                result.paths = o;
-//                return result;
-//            }
-//        }
+        @XmlRootElement(name = "paths")
+        static class PathsBean {
+
+            @XmlElement(name = "path")
+            public File[] paths;
+
+            static PathsBean create(File[] o) {
+                PathsBean result = new PathsBean();
+                result.paths = o;
+                return result;
+            }
+        }
     }
 
-//    private static final class ProvidersV2Step extends InstallerStep.LookupStep<TsProvider> {
-//
-//        final Preferences prefs = prefs();
-//        final Parser<File[]> pathsParser = Parsers.onJAXB(PathsBean.class).andThen(o -> o.paths != null ? o.paths : new File[0])::parse;
-//        final Formatter<File[]> pathsFormatter = Formatters.onJAXB(PathsBean.class, false).compose(PathsBean::create)::format;
-//
-//        ProvidersV2Step() {
-//            super(TsProvider.class);
-//        }
-//
-//        private void register(Iterable<? extends TsProvider> providers) {
-//            Preferences pathsNode = prefs.node("paths");
-//            for (TsProvider o : providers) {
-//                TsManager.getDefault().register(o);
-//                if (o instanceof FileLoader) {
-//                    tryGet(pathsNode, o.getSource(), pathsParser)
-//                            .ifPresent(((FileLoader) o)::setPaths);
-//                }
-//            }
-//        }
-//
-//        private void unregister(Iterable<? extends TsProvider> providers) {
-//            Preferences pathsNode = prefs.node("paths");
-//            for (TsProvider o : providers) {
-//                if (o instanceof FileLoader) {
-//                    tryPut(pathsNode, o.getSource(), pathsFormatter, ((FileLoader) o).getPaths());
-//                }
-//                TsManager.getDefault().unregister(o);
-//            }
-//        }
-//
-//        private static <X> List<X> except(List<X> l, List<X> r) {
-//            List<X> result = new ArrayList(l);
-//            result.removeAll(r);
-//            return result;
-//        }
-//
-//        private static String toString(Stream<? extends TsProvider> providers) {
-//            return providers
-//                    .map(o -> o.getSource() + "(" + o.getClass().getName() + ")")
-//                    .collect(Collectors.joining(", "));
-//        }
-//
-//        @Override
-//        protected void onResultChanged(Lookup.Result<TsProvider> lookup) {
-//            List<TsProvider> old = TsManager.getDefault().getProviders().collect(Collectors.toList());
-//            List<TsProvider> current = new ArrayList<>(lookup.allInstances());
-//
-//            unregister(except(old, current));
-//            register(except(current, old));
-//        }
-//
-//        @Override
-//        protected void onRestore(Lookup.Result<TsProvider> lookup) {
-//            register(lookup.allInstances());
-//            LOGGER.debug("Loaded providers: [{}]", toString(TsManager.getDefault().getProviders()));
-//        }
-//
-//        @Override
-//        protected void onClose(Lookup.Result<TsProvider> lookup) {
-//            unregister(TsManager.getDefault().getProviders().collect(Collectors.toList()));
-//            try {
-//                prefs.flush();
-//            } catch (BackingStoreException ex) {
-//                LOGGER.warn("Can't flush storage", ex);
-//            }
-//        }
-//
-//        @XmlRootElement(name = "paths")
-//        @XmlJavaTypeAdapter(value = FileXmlAdapter.class, type = File.class)
-//        static class PathsBean {
-//
-//            @XmlElement(name = "path")
-//            public File[] paths;
-//
-//            static PathsBean create(File[] o) {
-//                PathsBean result = new PathsBean();
-//                result.paths = o;
-//                return result;
-//            }
-//        }
-//    }
-    
     private static final class TsVariableStep extends InstallerStep {
 
         @Override
