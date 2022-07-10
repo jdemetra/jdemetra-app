1d63441b17dc4aa9b56ffdddd1ac2132ead73f3b
Jean Palate <jean.palate@nbb.be>
2022-07-10 09:33:25 +0200

    Bug in TsTopComponents

--- demetra-desktop-api/src/main/java/demetra/desktop/workspace/ui/WorkspaceTopComponent.java
+++ demetra-desktop-api/src/main/java/demetra/desktop/workspace/ui/WorkspaceTopComponent.java
@@ -6,6 +6,9 @@ package demetra.desktop.workspace.ui;
 
 import demetra.desktop.ui.ActiveView;
 import demetra.desktop.ui.ActiveViewManager;
+import demetra.desktop.ui.Menus;
+import demetra.desktop.workspace.WorkspaceFactory;
+import demetra.desktop.workspace.WorkspaceItem;
 import java.util.Collection;
 import javax.swing.Action;
 import javax.swing.JMenu;
@@ -15,36 +18,33 @@ import org.openide.util.Lookup;
 import org.openide.util.LookupEvent;
 import org.openide.util.LookupListener;
 import org.openide.windows.TopComponent;
-import demetra.desktop.workspace.WorkspaceItem;
-import demetra.desktop.workspace.WorkspaceFactory;
-import demetra.desktop.ui.Menus;
-import org.openide.nodes.Node;
 
 /**
  *
  * @author Jean Palate
  * @param <T>
  */
-public abstract class WorkspaceTopComponent<T> extends TopComponent implements ActiveView, ExplorerManager.Provider {
+public abstract class WorkspaceTopComponent<T> extends TopComponent implements ActiveView, ExplorerManager.Provider, LookupListener {
 
     protected final WorkspaceItem<T> doc;
-
+    private final Lookup.Result<WorkspaceFactory.Event> wsevent;
 
     protected WorkspaceTopComponent(WorkspaceItem<T> doc) {
         this.doc = doc;
+        this.wsevent = WorkspaceFactory.getInstance().getLookup().lookupResult(WorkspaceFactory.Event.class);
         setDisplayName(doc.getDisplayName());
     }
 
     public WorkspaceItem<T> getDocument() {
         return doc;
     }
-    
+
     @Override
-    public boolean hasContextMenu(){
+    public boolean hasContextMenu() {
         return true;
     }
-    
-    protected String getContextPath(){
+
+    protected String getContextPath() {
         return null;
     }
 
@@ -75,16 +75,32 @@ public abstract class WorkspaceTopComponent<T> extends TopComponent implements A
     public void componentOpened() {
         super.componentOpened();
         doc.setView(this);
+        wsevent.addLookupListener(this);
         // TODO add custom code on component opening
     }
 
     @Override
     public void componentClosed() {
+        wsevent.removeLookupListener(this);
         doc.setView(null);
         super.componentClosed();
     }
 
     public void refresh() {
     }
- 
+
+    @Override
+    public void resultChanged(LookupEvent le) {
+        Collection<? extends WorkspaceFactory.Event> all = wsevent.allInstances();
+        for (WorkspaceFactory.Event ev : all) {
+            if (ev.id.equals(doc.getId())) {
+                switch (ev.info) {
+                    case WorkspaceFactory.Event.ITEMCHANGED -> //if (ev.source != this) {
+                        SwingUtilities.invokeLater(this::refresh);
+                    //}
+                }
+            }
+        }
+    }
+
 }
