2c4dfefa36096bda191e26fefdebe282ba5ee6da
Philippe Charles <philippe.charles@nbb.be>
2024-02-08 11:49:53 +0100

    Fix warnings on annotation processing

--- nbdemetra-core/src/main/java/ec/nbdemetra/core/GlobalServiceProcessor.java
+++ nbdemetra-core/src/main/java/ec/nbdemetra/core/GlobalServiceProcessor.java
@@ -1,37 +1,34 @@
 /*
  * Copyright 2013 National Bank of Belgium
- * 
- * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
  * by the European Commission - subsequent versions of the EUPL (the "Licence");
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl
- * 
- * Unless required by applicable law or agreed to in writing, software 
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and 
+ * See the Licence for the specific language governing permissions and
  * limitations under the Licence.
  */
 package ec.nbdemetra.core;
 
-import java.util.Set;
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.Messager;
-import javax.annotation.processing.Processor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
+import org.openide.util.lookup.ServiceProvider;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.ExecutableType;
 import javax.tools.Diagnostic;
-import org.openide.util.lookup.ServiceProvider;
+import java.util.Set;
 
 /**
- *
  * @author Philippe Charles
  */
 @ServiceProvider(service = Processor.class)
@@ -70,6 +67,11 @@ public final class GlobalServiceProcessor extends AbstractProcessor {
             }
         }
         return true;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
     }
 
     private Element getDefaultMethod(TypeElement typeElement) {
